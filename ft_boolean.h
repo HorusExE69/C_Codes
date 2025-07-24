af1c6448bf771569b9bf8900f756c08e78f6895b
Hans Bretagne <hbretagn@z4r9p2.42lyon.fr>
2025-07-24 16:07:02 +0200

    added

--- ft_boolean.h
+++ ft_boolean.h
@@ -10,7 +10,7 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifdef FT_BOOLEAN_H
+#ifndef FT_BOOLEAN_H
 # define FT_BOOLEAN_H
 # include <unistd.h>
 
@@ -18,11 +18,11 @@
 
 # define FALSE 0
 
-# define EVEN_MSG "I have an even number of arguments."
+# define EVEN_MSG "I have an even number of arguments.\n"
 
-# define ODD_MSG "I have an odd number of arguments."
+# define ODD_MSG "I have an odd number of arguments.\n"
 
-# define SUCCESS 1
+# define SUCCESS 0
 
 # define EVEN(nb) (nb%2==0)
 
