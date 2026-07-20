5ca663d00d064afd9d6bc921aace371df3e3c42c
HorusExE69 <131947415+HorusExE69@users.noreply.github.com>
2026-07-20 14:20:02 +0200

    Update ft_strstr.c

--- C/ft_strstr.c
+++ C/ft_strstr.c
@@ -6,52 +6,26 @@
 /*   By: hbretagn <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/16 13:39:25 by hbretagn          #+#    #+#             */
-/*   Updated: 2025/07/16 13:39:27 by hbretagn         ###   ########.fr       */
+/*   Updated: 2025/07/20 13:00:00 by hbretagn         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
-int	len(char *str)
-{
-	int	n;
-
-	n = 0;
-	while (str[n] != '\0')
-	{
-		n ++;
-	}
-	return (n);
-}
-
-char	*to_find_str(char *str, char *to_find, int i, int j)
-{
-	while (str[i] != '\0')
-	{
-		to_find[j] = str[i];
-		j ++;
-		i ++;
-	}
-	to_find[j] = '\0';
-	return (to_find);
-}
 
 char	*ft_strstr(char *str, char *to_find)
 {
 	int	i;
 	int	j;
-	int	len_str;
 
+	if (to_find[0] == '\0')
+		return (str);
 	i = 0;
-	j = 0;
-	len_str = len(str);
-	while (i != len_str + 1)
+	while (str[i] != '\0')
 	{
-		if (to_find[j] && to_find[j] == str[i])
-			j ++;
-		else if (!to_find[j])
-			return (to_find_str(str, to_find, i, j));
-		else
-			j = 0;
-		i ++;
+		j = 0;
+		while (to_find[j] != '\0' && str[i + j] == to_find[j])
+			j++;
+		if (to_find[j] == '\0')
+			return (&str[i]);
+		i++;
 	}
 	return (0);
 }
